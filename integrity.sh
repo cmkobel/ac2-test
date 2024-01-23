01b20bd588bd60853338bed7eac33c5811bcd119
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-01-23 12:28:58 +0100

    amend

--- integrity.sh
+++ integrity.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 set -x
 
-[ -d results_ac2 ] && /usr/bin/rm -r results_ac2 || exit 1
+# Clean up potential old outputs.
+[ -d results_ac2 ] && /usr/bin/rm -r results_ac2
+[ -d scripts_temporary ] && /usr/bin/rm -r scripts_temporary
 
 
+# Check versions and get stats
 which mamba
 mamba --version
 
@@ -13,13 +16,10 @@ which apptainer
 apptainer version
 
 apptainer cache list
-
-
 du -sh ~/.asscom2
 
 # Install ac2
-
-mamba create -y -c conda-forge -c bioconda -n asscom2_test assemblycomparator2=2.5.14 # TODO set to patch17
+mamba create -y -c conda-forge -c bioconda -n asscom2_test assemblycomparator2=2.5.14 # TODO set to patch 17
 
 source activate asscom2_test
 
