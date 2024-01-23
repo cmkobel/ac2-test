2513733ad0068f1554c12373fc0013b2a63dc049
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-01-23 12:22:50 +0100

    first

---
+++ integrity.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+
+
+which mamba
+mamba --version
+
+which apptainer
+apptainer version
+
+apptainer cache list
+
+
+du -sh ~/.asscom2
+
+# Install ac2
+
+#mamba create -y -c conda-forge -c bioconda -n asscom2_test assemblycomparator2=2.5.14 # TODO set to patch17
+
+source activate asscom2_test
+
+which asscom2
+
+
+# Copy in test files
+echo $CONDA_PREFIX
+
+cp $CONDA_PREFIX/assemblycomparator2/tests/MAGs/*.fasta .
+
+
+# Run comprehensive?? test
+asscom2 --until fast
+
+exit_code=$?
+
+echo $exit_code
+
+
+# Assert that outputs are corret
+exit $exit_code
